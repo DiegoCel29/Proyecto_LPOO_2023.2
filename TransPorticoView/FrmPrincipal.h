6ee13d2a5ffde613820849ee271859677b331901
Diego Celis <diegoal.celis@gmail.com>
2023-09-19 16:04:56 -0500

    V1.0.6

--- TransPorticoView/FrmPrincipal.h
+++ TransPorticoView/FrmPrincipal.h
@@ -48,11 +48,19 @@ namespace TransPorticoView {
 		/// </summary>
 		void InitializeComponent(void)
 		{
-			this->components = gcnew System::ComponentModel::Container();
-			this->Size = System::Drawing::Size(300,300);
-			this->Text = L"FrmPrincipal";
-			this->Padding = System::Windows::Forms::Padding(0);
+			this->SuspendLayout();
+			// 
+			// FrmPrincipal
+			// 
+			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->ClientSize = System::Drawing::Size(558, 469);
+			this->IsMdiContainer = true;
+			this->Name = L"FrmPrincipal";
+			this->SizeGripStyle = System::Windows::Forms::SizeGripStyle::Show;
+			this->Text = L"FrmPrincipal";
+			this->ResumeLayout(false);
+
 		}
 #pragma endregion
 	};
