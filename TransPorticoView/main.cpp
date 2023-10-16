5de6bc72453cf218306ac712fc778eedf2d10b34
Sulta <Sulta@CESAR_ED_PEA>
2023-10-15 21:37:53 -0500

    Si

--- TransPorticoView/main.cpp
+++ TransPorticoView/main.cpp
@@ -1,4 +1,4 @@
-#include "AtajoMantenimiento.h"
+#include "PantallaIngreso.h"
 
 using namespace System;
 using namespace System::Windows::Forms;
@@ -8,6 +8,6 @@ void main(array <String^>^ args) {
 	Application::EnableVisualStyles();
 	Application::SetCompatibleTextRenderingDefault(false);
 
-	AtajoMantenimiento Ventana;
+	PantallaIngreso Ventana;
 	Application::Run(% Ventana);
 }
